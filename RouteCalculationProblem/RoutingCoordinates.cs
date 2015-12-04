f76211d1b5bec4ee8a3b02180354b3b907cfdf33
vladharani <vlad.harani@live.com>
2015-12-04 09:44:44 +0200

    Reparat, sa treaca testul, voi incerca problema cu Coloanele cu struct, apoi trec la aceasta, struct si apoi punctul B al problemei

--- RouteCalculationProblem/RoutingCoordinates.cs
+++ RouteCalculationProblem/RoutingCoordinates.cs
@@ -10,9 +10,10 @@ namespace RoutingPointProblem
         {
             int endX;
             int endY;
-            string[] commands = new string[] { "up" };
-            Assert.AreEqual(0, FindLastPoint(0,0,["up"],out endX, out endY));
-            Assert.AreEqual(0, endY);
+            string[] commands = new string[] { "up", "right","up","right" };
+            FindLastPoint(0,0, commands, out endX, out endY);
+            Assert.AreEqual(2, endX);
+            Assert.AreEqual(2, endY);
         }
         void FindLastPoint(int startX, int startY, string[] commands, out int endX, out int endY)
         {
@@ -21,10 +22,10 @@ namespace RoutingPointProblem
             endY = 0;
             for (int index = 0; index < commands.Length; index++)
             {
-                if (commands[index] == "up") { endY = startY + 1; }
-                else if (commands[index] == "down") { endY = startY - 1; }
-                else if (commands[index] == "right") { endX = startX + 1; }
-                else if (commands[index] == "left") { endX = startX + 1; }
+                if (commands[index] == "up") { endY = startY + 1; startY = endY; }
+                else if (commands[index] == "down") { endY = startY - 1; startY = endY; }
+                else if (commands[index] == "right") { endX = startX + 1; startX = endX; }
+                else if (commands[index] == "left") { endX = startX + 1; startX = endX; }
             }
         }
     }
